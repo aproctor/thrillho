b95f08714153d2595b7979ceca0ff17b651e68d6
Adam Proctor <aproctor@gmail.com>
2014-04-28 12:27:36 -0400

    initial gopher setup

--- Assets/Scripts/Gameplay/Monster.cs
+++ Assets/Scripts/Gameplay/Monster.cs
@@ -24,6 +24,12 @@ public class Monster : Unit {
     }
   }
 
+	public override void Die ()
+	{
+		//Don't call base.Die() here that will destroy it
+		state = MonsterStates.Dead;
+	}
+
   public virtual void OnAggro() {
     //Override this to allow shooting
   }
